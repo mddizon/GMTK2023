3a1e34e9692ba7528b9ff359f655c80b6140e865
Mark Dizon <markdizon07@gmail.com>
2023-07-08 11:52:17 -0700

    Add timer

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,17 +4,22 @@ extends Node2D
 @onready var spawn_position = $ShipSpawnPosition
 @onready var bullet_container = $BulletContainer
 @onready var unit_container = $UnitContainer
-@onready var bomb_counter = $Control/PanelContainer/Rows/ColorRect3/BombRows/BombColumns
-@onready var resource_counter = $Control/PanelContainer/Rows/ColorRect/VBoxContainer/Resource
+@onready var bomb_counter = $Control/PanelContainer/Rows/BombContainer/BombRows/BombColumns
+@onready var resource_counter = $Control/PanelContainer/Rows/ResourceContainer/VBoxContainer/Resource
 
 #This sucks but whatever
-@onready var bug_row_1 = $Control/PanelContainer/Rows/ColorRect/VBoxContainer/BugMarket/BugRow1
-@onready var bug_row_2 = $Control/PanelContainer/Rows/ColorRect/VBoxContainer/BugMarket/BugRow2
+@onready var bug_row_1 = $Control/PanelContainer/Rows/ResourceContainer/VBoxContainer/BugMarket/BugRow1
+@onready var bug_row_2 = $Control/PanelContainer/Rows/ResourceContainer/VBoxContainer/BugMarket/BugRow2
+
+#Timer
+@onready var timer = $GameOverTimer
+@onready var gameover_label = $Control/PanelContainer/Rows/DistanceContainer/VBoxContainer/DistanceValue
 
 @export var units: Array[BugStats] = []
 @export var num_bombs = 3
 @export var resources = 100
 @export var spawn_cooldown = 1
+@export var total_distance = 5000
 
 var bomb_texture = preload("res://assets/HumanShip/BombCounter.png")
 var Unit = preload("res://scenes/unit.tscn")
@@ -40,6 +45,8 @@ func _process(_delta):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+	
+	updateDistanceLabel()
 
 func _on_ship_fired(laser_scene, location):
 	var laser = laser_scene.instantiate()
@@ -85,4 +92,11 @@ func updateBombLabel():
 func updateResourceLabel():
 	resource_counter.text = "Larvae: %s" % resources
 
+func updateDistanceLabel():
+	var remainingTime = timer.wait_time - timer.time_left
+	var parsecs = total_distance - remainingTime/timer.wait_time * total_distance
+	gameover_label.text = "%d Parsecs" % parsecs
 
+func _on_game_over_timer_timeout():
+	if num_bombs >= 0:
+		print('You Lose!')
