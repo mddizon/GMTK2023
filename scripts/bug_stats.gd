93a6968aff3b15dc7b4144084c4da675aff0346f
Mark Dizon <markdizon07@gmail.com>
2023-07-08 20:18:43 -0700

    Update drop rate to be a float

--- scripts/bug_stats.gd
+++ scripts/bug_stats.gd
@@ -8,5 +8,5 @@ class_name BugStats
 @export var shot_cooldown = 1
 @export var shot_speed = 200
 @export var powerup: Array[GlobalTypes.Powers] = [GlobalTypes.Powers.FIRE_RATE]
-@export var drop_rate = 0
+@export var drop_rate = 0.0
 @export var img: Texture = null
